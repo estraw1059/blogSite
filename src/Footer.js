e4fbe9a4a02bc41f91119ef20e6b3c0671cdda0a
unknown <estraw@smu.edu>
2023-04-23 15:52:06 -0500

    Fixed Footer Styling

--- src/Footer.js
+++ src/Footer.js
@@ -15,13 +15,13 @@ const Footer = () => {
         <Row>
             <Col>
                 <div className='iconContainer'>
-                    <Button className='icon' style={{ outline: 'none' }} href='mailto:estraw@smu.edu'>
+                    <Button variant='dark' className='bg-dark icon' href='mailto:estraw@smu.edu'>
                         <img src={emailSVG} alt="email me" />
                     </Button>
-                    <Button className='bg-dark icon' style={{ outline: 'none !important' }} href="https://github.com/estraw1059" target="_blank">
+                    <Button variant='dark' className='bg-dark icon' href="https://github.com/estraw1059" target="_blank">
                         <img src={githubSVG} alt="github" />
                     </Button>
-                    <Button className='bg-dark icon' style={{ outline: 'none !important' }} href="https://www.linkedin.com/in/eric-straw-477739102" target="_blank">
+                    <Button variant='dark' className='bg-dark icon' href="https://www.linkedin.com/in/eric-straw-477739102" target="_blank">
                         <img src={LinkdenIn} alt="LinkdenIn" />
                     </Button>
                 </div>
