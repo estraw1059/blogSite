533b7b4a2e92cc31e354c7059943491cf7985137
unknown <estraw@smu.edu>
2023-05-02 17:13:54 -0500

    Update Homepage to show under construction

---
+++ src/Components/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Image} from 'react-bootstrap';
+import './UnderConstruction.css';
+
+
+const UnderConstruction = () => {
+  return (
+    <div className="under-construction">
+      <Image src="https://img.icons8.com/officel/80/null/under-construction.png" fluid/>
+      <h2>Site Under Construction</h2>
+      <p>We're sorry, this site is currently under construction. Please check back later!</p>
+    </div>
+  );
+};
+
+export default UnderConstruction;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,12 @@
 /* eslint-disable-next-line */
 import React from 'react';
-import BlogPostCard from '../BlogPostCard';
-import ConstructionSite from '../ConstructionSite';
+import UnderConstruction from '../Components/UnderConstruction/UnderConstruction';
 import {Container, Image} from 'react-bootstrap';
+import './Pages.css';
 
 const Home = () => {
     return (
-        <div>
-            <ConstructionSite/>
-                <Image src="https://img.icons8.com/officel/80/null/under-construction.png" fluid/>
-            <Container>
-                <BlogPostCard title ="Example Title" text="A very basic description of my Blog Post"/>
-            </Container>
-        </div>
+        <UnderConstruction/>
     );
 };
 
