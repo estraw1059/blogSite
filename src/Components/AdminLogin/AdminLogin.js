e67e8c138bb972550bdddeeb2fb3d8904fd672ec
Eric <estraw@smu.edu>
2023-06-14 05:59:07 -0500

    Adjusted Login page to have logout button now

--- src/Components/AdminLogin/AdminLogin.js
+++ src/Components/AdminLogin/AdminLogin.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import {auth} from "../../Firebase";
 import { useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { Button, Container, Row } from "react-bootstrap";
 
 const AdminLogin = () => {
     const [email, setEmail] = useState('');
@@ -27,16 +28,29 @@ const AdminLogin = () => {
       signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
           // Login successful, do something with the user data
-          const user = userCredential.user;
-          console.log('Logged in:', user);
           handleRedirect();
         })
         .catch((error) => {
           // Handle login errors
           console.error('Login error:', error.message);
+          
         });
     };
+
+    const logout = () => {
+      signOut(auth).then(() => {
+        navigate('/admin/login')
+      })
+    }
   
+    if (auth.currentUser != null) {
+      return (
+        <Container>
+            <Button onClick={logout} className='m-2'>Log Out</Button>
+        </Container>
+      )
+    }
+
     return (
       <div>
         <h2>Login</h2>
