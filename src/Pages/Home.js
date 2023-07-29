48a63cce8c6ae2514b62cb2dc6857e9e89d93774
Eric <estraw@smu.edu>
2023-07-30 08:38:55 +1200

    Added Carousel to home page. Need to link in Blog Post teaser template next

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,21 @@
 /* eslint-disable-next-line */
 import React from 'react';
 import UnderConstruction from '../Components/UnderConstruction/UnderConstruction';
-import {Container, Image} from 'react-bootstrap';
+import { Container, Carousel } from 'react-bootstrap';
 import './Pages.css';
 
 const Home = () => {
     return (
-        <UnderConstruction/>
+            <Carousel className='homePageCarousel'>
+                <Carousel.Item>
+                    <UnderConstruction/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div>
+                        Second
+                    </div>
+                </Carousel.Item>
+            </Carousel>
     );
 };
 
