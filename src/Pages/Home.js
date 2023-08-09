6817d857ce228b8c308588d415b5b7e9cb66f7cd
Eric <estraw@smu.edu>
2023-08-09 06:02:26 -0500

    Got promoted post on main page. Now need to add it to the carousel Items

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,13 +5,30 @@ import { Carousel } from 'react-bootstrap';
 import './Pages.css';
 import SingleBlogPostAd from '../Components/SingleBlogPostAd/SingleBlogPostAd';
 import { useEffect } from 'react';
+import db from '../Firebase';
+import { collection, getDocs, query, where, limit} from 'firebase/firestore';
 
 const Home = () => {
     const [promotedPost, setPromotedPost] = useState([]);
     useEffect(() => {
-        //TODO: Get All Promoted Post
+        const getPromotedPost = async () => {
+            try {
+                const q = query(collection(db, 'blogPost'), limit(5), where('promoted', '==', true));
+                const querySnapshot = await getDocs(q);
+                const cardList = [];
+                querySnapshot.forEach((doc) => {
+                    cardList.push(doc.data());
+                });
+                setPromotedPost(cardList);
+
+            } catch (error) {
+                console.error('Error getting blog post', error);
+            }
+        }
+        getPromotedPost();
     }, []);
 
+    console.log(`${promotedPost[0].id}`);
     return (
             <Carousel className='homePageCarousel'>
                 <Carousel.Item>
